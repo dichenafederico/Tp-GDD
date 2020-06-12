5c7b1059f777b2e6a515cea74c3240af10d2777a
FrancoCingolani <cingolani_franco@hotmail.com>
2020-06-12 20:39:45 -0300

    Algunos fix en la migración

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -2,54 +2,48 @@ USE [GD1C2020]
 GO
 
 
---COSAS A HACER:
---Creación de Esquema (y sus drop previos)
---Creación de Tablas (y sus drop previos)
---OPCIONAL: Creación de triggers
---OPCIONAL: Creación de vistas
---OPCIONAL: Creación de funciones escalares
---Creación de stored procedures para llenar las tablas del DER
---Ejecución de stored procedures anteriores
---Drop de tablas reales auxiliares. Deben quedar solo las tablas del DER
+-------------------------------------------------------------------------------------------------
+-----------------------------------CREACIÃ“N SCHEMA-----------------------------------------------
+-------------------------------------------------------------------------------------------------
+
+GO
+	CREATE SCHEMA [SELECT_BEST_TEAM_FROM_CUARENTENA] --Buscar cuÃ¡l es el usuario correcto con el que crear el schema
+GO
+
 
 -------------------------------------------------------------------------------------------------
 -----------------------------------DROP DE TABLES------------------------------------------------
 -------------------------------------------------------------------------------------------------
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].RUTA_AEREA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[RUTA_AEREA];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].VUELO', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[VUELO];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].AVION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[AVION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].AEROLINEA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[AEROLINEA];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].PASAJE', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[PASAJE];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].VENTA_PASAJE', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[VENTA_PASAJE];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].COMPRA_PASAJE', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[COMPRA_PASAJE];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].BUTACA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[BUTACA];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].COMPRA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[COMPRA];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].COMPRA_HABITACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[COMPRA_HABITACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].HABITACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[HABITACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].ESTADIA_HABITACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[ESTADIA_HABITACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].TIPO_HABITACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[TIPO_HABITACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].HOTEL', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[HOTEL];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].TIPO_OPERACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[TIPO_OPERACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].VENTA_HABITACION', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[VENTA_HABITACION];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].VENTA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[VENTA];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].CLIENTE', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[CLIENTE];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].SUCURSAL', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[SUCURSAL];
-IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].FACTURA', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[FACTURA];
+
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Venta_Habitacion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Venta_Habitacion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Compra_Estadias', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Compra_Estadias];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Compra_Pasaje', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Compra_Pasaje];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Compra', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Compra];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Venta_Pasaje', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Venta_Pasaje];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Factura', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Factura];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Venta', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Venta];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Estadia_Habitacion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Estadia_Habitacion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Estadia', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Estadia];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Habitacion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Habitacion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Habitacion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Tipo_Habitacion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Hotel];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Cliente', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Cliente];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Sucursal', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Sucursal];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca_Avion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Butaca_Avion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Pasaje];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Butaca];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Vuelo', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Vuelo];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Avion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Avion];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Aerolinea', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Aerolinea];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Ruta_Aerea', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Ruta_Aerea];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Ciudad', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Ciudad];
+IF OBJECT_ID('[SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Operacion', 'U') IS NOT NULL DROP TABLE SELECT_BEST_TEAM_FROM_CUARENTENA.[Tipo_Operacion];
+
+
 
 
 -------------------------------------------------------------------------------------------------
------------------------------------CREACIÓN SCHEMA-----------------------------------------------
--------------------------------------------------------------------------------------------------
-
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'SELECT_BEST_TEAM_FROM_CUARENTENA')
-BEGIN
-	EXEC ('CREATE SCHEMA [SELECT_BEST_TEAM_FROM_CUARENTENA] AUTHORIZATION gd') --Buscar cuál es el usuario correcto con el que crear el schema
-END
-GO
-
--------------------------------------------------------------------------------------------------
------------------------------------CREACIÓN DE TABLAS--------------------------------------------
+-----------------------------------CREACIÃ“N DE TABLAS--------------------------------------------
 -------------------------------------------------------------------------------------------------
 
 
@@ -91,23 +85,24 @@ CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Vuelo(
 	)
 GO
 
-CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje(
-	id_pasaje integer IDENTITY PRIMARY KEY,
-	pasaje_codigo numeric(18, 0) NOT NULL,
-	pasaje_costo numeric(18, 2) NOT NULL,
-	pasaje_precio numeric(18, 2) NOT NULL,
-	pasaje_fecha_compra datetime NOT NULL,
-	id_avion integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Avion(id_avion),
-	id_butaca integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca(id_butaca)
-	)
-GO
-
 CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca(
 	id_butaca integer IDENTITY PRIMARY KEY,
 	butaca_numero numeric(18, 0) NOT NULL,
 	butaca_tipo nvarchar(255) NOT NULL
 	)
 GO
+
+CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje(
+	id_pasaje integer IDENTITY PRIMARY KEY,
+	pasaje_codigo numeric(18, 0) NOT NULL,
+	pasaje_costo numeric(18, 2) NOT NULL,
+	pasaje_precio numeric(18, 2) NOT NULL,
+	pasaje_fecha_compra datetime NULL,
+	id_avion integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Avion(id_avion),
+	id_butaca integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca(id_butaca)
+	)
+GO
+
 
 CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca_Avion(
 	id_butaca integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca(id_butaca),
@@ -193,13 +188,13 @@ CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Habitacion(
 	)
 GO
 
-CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra(
+CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra( 
 	id_compra integer IDENTITY PRIMARY KEY,
 	compra_numero numeric(18, 0) NOT NULL,
 	compra_fecha datetime NOT NULL,
 	id_tipo_operacion integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Operacion(id_tipo_operacion),
-	id_aerolinea integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Aerolinea(id_aerolinea),
-	id_hotel integer NOT NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel(id_hotel)
+	id_aerolinea integer NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Aerolinea(id_aerolinea), --Lo hice nulleable
+	id_hotel integer NULL FOREIGN KEY REFERENCES [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel(id_hotel) -- Lo hice nulleable
 	)
 GO
 
@@ -241,3 +236,188 @@ CREATE TABLE [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta_Habitacion(
 	)
 GO
 
+
+--------------------------------------------------------------
+-------------------MigraciÃ³n de los datos---------------------
+--------------------------------------------------------------
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Operacion (descripcion) values
+	('Estadia'),
+	('Pasaje')
+GO	
+
+SELECT * FROM gd_esquema.Maestra
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Ciudad (ciudad_nombre)
+	SELECT DISTINCT RUTA_AEREA_CIU_ORIG 
+	FROM gd_esquema.Maestra 
+	WHERE RUTA_AEREA_CIU_ORIG IS NOT NULL
+GO	
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Ruta_Aerea (ruta_aerea_codigo,ruta_aerea_ciu_orig,ruta_aerea_ciu_dest)
+	SELECT DISTINCT RUTA_AEREA_CODIGO,
+		(SELECT id_ciudad FROM SELECT_BEST_TEAM_FROM_CUARENTENA.Ciudad c WHERE m.ruta_aerea_ciu_orig = c.ciudad_nombre),
+		(SELECT id_ciudad FROM SELECT_BEST_TEAM_FROM_CUARENTENA.Ciudad c WHERE m.ruta_aerea_ciu_dest = c.ciudad_nombre)
+	FROM gd_esquema.Maestra m
+	WHERE RUTA_AEREA_CODIGO IS NOT NULL
+GO
+
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Aerolinea (aerolinea_razon_social)
+	SELECT DISTINCT EMPRESA_RAZON_SOCIAL
+	FROM gd_esquema.Maestra Where HOTEL_CALLE IS NULL
+GO
+
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Avion (avion_modelo,avion_identificador,id_aerolinea)
+	SELECT DISTINCT AVION_MODELO,AVION_IDENTIFICADOR, 
+		(SELECT id_aerolinea FROM SELECT_BEST_TEAM_FROM_CUARENTENA.Aerolinea a WHERE a.aerolinea_razon_social = m.EMPRESA_RAZON_SOCIAL)
+	FROM gd_esquema.Maestra m
+	WHERE AVION_MODELO IS NOT NULL AND AVION_IDENTIFICADOR IS NOT NULL
+GO
+
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Vuelo (vuelo_codigo,vuelo_fecha_salida,vuelo_fecha_llegada,id_avion,id_ruta_aerea)
+	SELECT DISTINCT VUELO_CODIGO,VUELO_FECHA_SALUDA,VUELO_FECHA_LLEGADA,a.id_avion,r.id_ruta_aerea
+	FROM gd_esquema.Maestra m
+	 join [SELECT_BEST_TEAM_FROM_CUARENTENA].Ruta_Aerea r
+	 on r.ruta_aerea_codigo = m.RUTA_AEREA_CODIGO 
+	 join [SELECT_BEST_TEAM_FROM_CUARENTENA].Avion a
+	 on m.AVION_IDENTIFICADOR = a.avion_identificador 
+	 where m.RUTA_AEREA_CODIGO IS NOT NULL
+GO
+
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca (butaca_numero,butaca_tipo)
+	SELECT DISTINCT BUTACA_NUMERO,BUTACA_TIPO
+	FROM gd_esquema.Maestra m
+	WHERE BUTACA_NUMERO IS NOT NULL AND BUTACA_TIPO IS NOT NULL
+	ORDER BY BUTACA_NUMERO
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje (pasaje_codigo,pasaje_costo,pasaje_precio,pasaje_fecha_compra,id_avion,id_butaca) 
+	SELECT DISTINCT m.PASAJE_CODIGO,m.PASAJE_COSTO,m.PASAJE_PRECIO,m.PASAJE_FECHA_COMPRA, a.id_avion, b.id_butaca 
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Avion a
+	on m.AVION_IDENTIFICADOR = a.avion_identificador 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca b
+	on b.butaca_numero = m.BUTACA_NUMERO	
+	where m.RUTA_AEREA_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Butaca_Avion (id_butaca, id_avion)
+	SELECT DISTINCT p.id_butaca,p.id_avion	
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje p
+	on m.PASAJE_CODIGO = p.pasaje_codigo
+	WHERE m.PASAJE_CODIGO IS NOT NULL	
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Sucursal (sucursal_dir,sucursal_mail,sucursal_telefono)
+	SELECT DISTINCT SUCURSAL_DIR,SUCURSAL_MAIL,SUCURSAL_TELEFONO
+	FROM gd_esquema.Maestra
+	WHERE SUCURSAL_DIR IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Cliente (cliente_apellido,cliente_nombre,cliente_dni,cliente_fecha_nac,cliente_mail,cliente_telefono)
+	SELECT DISTINCT CLIENTE_APELLIDO,CLIENTE_NOMBRE,CLIENTE_DNI,CLIENTE_FECHA_NAC,CLIENTE_MAIL,CLIENTE_TELEFONO
+	FROM gd_esquema.Maestra
+	WHERE CLIENTE_DNI IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel (hotel_calle,hotel_nro_calle,hotel_razon_social)
+	SELECT DISTINCT HOTEL_CALLE,HOTEL_NRO_CALLE, EMPRESA_RAZON_SOCIAL
+	FROM gd_esquema.Maestra Where HOTEL_CALLE IS NOT null
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Habitacion (tipo_habitacion_codigo,tipo_habitacion_desc)
+	SELECT DISTINCT TIPO_HABITACION_CODIGO,TIPO_HABITACION_DESC
+	FROM gd_esquema.Maestra 
+	WHERE TIPO_HABITACION_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Habitacion (habitacion_numero,habitacion_piso,habitacion_frente,habitacion_costo,habitacion_precio,id_hotel,id_tipo_habitacion)
+	SELECT DISTINCT HABITACION_NUMERO,HABITACION_PISO,HABITACION_FRENTE,HABITACION_COSTO,HABITACION_PRECIO,h.id_hotel,th.id_tipo_habitacion
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel h
+	on m.EMPRESA_RAZON_SOCIAL = h.hotel_razon_social
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Tipo_Habitacion th
+	on m.TIPO_HABITACION_CODIGO = th.tipo_habitacion_codigo
+	where m.HOTEL_CALLE IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Estadia (estadia_fecha,estadia_checkin,estadia_checkout,estadia_cantidad_noches,estadia_codigo)
+	SELECT DISTINCT ESTADIA_FECHA_INI,ESTADIA_FECHA_INI, (DATEADD(dd, estadia_cantidad_noches, ESTADIA_FECHA_INI)),ESTADIA_CANTIDAD_NOCHES,ESTADIA_CODIGO 
+	FROM gd_esquema.Maestra 
+	where ESTADIA_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta (venta_numero,id_tipo_operacion,venta_fecha,id_sucursal,id_cliente)
+	SELECT DISTINCT FACTURA_NRO, (CASE WHEN HOTEL_CALLE IS NULL THEN 2 ELSE 1 END), FACTURA_FECHA, s.id_sucursal, c.id_cliente
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Sucursal s
+	on s.sucursal_dir = m.SUCURSAL_DIR
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Cliente c
+	on c.cliente_dni = m.CLIENTE_DNI
+	where m.cliente_dni IS NOT NULL AND m.sucursal_dir IS NOT NULL
+GO
+
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Factura (factura_fecha,factura_nro,id_venta)
+	SELECT DISTINCT FACTURA_FECHA,FACTURA_NRO, v.id_venta
+	FROM gd_esquema.Maestra
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta v
+	on FACTURA_NRO = v.venta_numero
+	where FACTURA_NRO IS NOT NULL 
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta_Pasaje (id_venta,id_pasaje)
+	SELECT DISTINCT  v.id_venta, p.id_pasaje
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta v
+	on v.venta_numero = m.FACTURA_NRO 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje p
+	on p.pasaje_codigo = m.PASAJE_CODIGO
+	where m.FACTURA_NRO IS NOT NULL AND m.PASAJE_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra (compra_numero,compra_fecha,id_tipo_operacion,id_aerolinea,id_hotel)
+	SELECT DISTINCT COMPRA_NUMERO, COMPRA_FECHA, (CASE WHEN m.HOTEL_CALLE = NULL THEN 2 ELSE 1 END), a.id_aerolinea, h.id_hotel
+	FROM gd_esquema.Maestra m
+	left join [SELECT_BEST_TEAM_FROM_CUARENTENA].Aerolinea a
+	on a.aerolinea_razon_social = m.EMPRESA_RAZON_SOCIAL AND m.HOTEL_CALLE IS NULL 
+	left join [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel h
+	on h.hotel_razon_social = m.EMPRESA_RAZON_SOCIAL AND m.HOTEL_CALLE IS NOT NULL	
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra_Pasaje (id_compra,id_pasaje)
+	SELECT DISTINCT c.id_compra, p.id_pasaje 
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra c
+	on c.compra_numero = m.COMPRA_NUMERO 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Pasaje p
+	on p.pasaje_codigo = m.PASAJE_CODIGO
+	where m.PASAJE_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra_Estadias (id_compra,id_estadia) 
+	SELECT DISTINCT c.id_compra, e.id_estadia 
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Compra c
+	on c.compra_numero = m.COMPRA_NUMERO 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Estadia e
+	on e.estadia_codigo = m.ESTADIA_CODIGO
+	where m.ESTADIA_CODIGO IS NOT NULL
+GO
+
+INSERT INTO [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta_Habitacion (id_venta,id_habitacion)
+	SELECT  v.id_venta, h.id_habitacion
+	FROM gd_esquema.Maestra m
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Venta v
+	on v.venta_numero = m.FACTURA_NRO 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Habitacion h
+	on h.habitacion_numero = m.HABITACION_NUMERO 
+	join [SELECT_BEST_TEAM_FROM_CUARENTENA].Hotel hot
+	on hot.id_hotel = h.id_hotel  
+	where m.FACTURA_NRO IS NOT NULL AND m.HOTEL_CALLE IS NOT NULL
+GO
